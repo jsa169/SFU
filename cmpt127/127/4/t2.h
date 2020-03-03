78f3b52d742687516301576b78478936be363a7b
Jack <jsa169@sfu.ca>
2020-03-03 15:08:47 -0800

    8

--- cmpt127/127/4/t2.h
+++
@@ -1,8 +0,0 @@
-void draw_rectangle( uint8_t array[], 
-		          unsigned int cols, 
-		          unsigned int rows, 
-		          int x,
-		          int y,
-		          int rect_width,
-		          int rect_height,
-		          uint8_t color );
--- cmpt127/127/5/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "intarr.h"
-
-
-void main(){
-	intarr_create(100112);
-	intarr_create(0);
-}
