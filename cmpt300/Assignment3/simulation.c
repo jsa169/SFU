293b7399dbb5e6225dbef0f9a1b39180e6d1df27
Jack <jsa169@sfu.ca>
2020-03-18 19:51:29 -0700

    3rd commit, added more functions

--- cmpt300/Assignment3/simulation.c
+++ cmpt300/Assignment3/simulation.c
@@ -1,7 +1,9 @@
 //simulation.c
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "function.h"
+
 
 
 LIST * ReadyQ_High;
@@ -13,13 +15,32 @@ int proc_count;
 PCB * current_proc;
 int parameter;
 
+PCB * SearchAll(int PID){
+	PCB * Pinfo;
 
-void create(int priority, int status){
+	if(PID == current_proc->PID) {
+		Pinfo = current_proc;
+	}else {
+		ListFirst(ReadyQ_High);
+		Pinfo = ListSearch(ReadyQ_High, comparator, &PID);
+		if (Pinfo == NULL) {
+			ListFirst(ReadyQ_Norm);
+			Pinfo = ListSearch(ReadyQ_Norm, comparator, &PID);
+			if(Pinfo == NULL){
+				ListFirst(ReadyQ_Low);
+				Pinfo = ListSearch(ReadyQ_Low, comparator, &PID);
+			}
+		}
+	}
+	return Pinfo;
+}
+
+PCB * create(int priority, int status){
 
 	PCB * new_proc = new_PCB(proc_count+1, priority, status);
 	if(new_proc == NULL) {
 		printf("Process Creation failed.\n");
-		return;
+		return NULL;
 	}
 	proc_count++;
 
@@ -29,34 +50,37 @@ void create(int priority, int status){
 
 	if (proc_count != 1) printf("Proccess Created: \n");
 	printPCB(new_proc);
+	return new_proc;
 
 }
 
 void Fork(){
-
-}
-
-void Kill(int PID){
-
-}
-
-void Exit(){
-
+	if(current_proc->PID == 1){
+		printf("Cannot fork initial process.\n");
+		return;
+	}
+	PCB * fork_proc = create(current_proc->priority,current_proc->status);
+	memcpy(fork_proc, current_proc, sizeof(PCB));
+	fork_proc->PID = proc_count;
+	fork_proc->status = 0;
+	printf("Successfully forked: \n");
+	printPCB(fork_proc);
 }
 
 void Quantum(){
 	int a;
-	if (current_proc->status == -1) ListPrepend(ReadyQ_High, current_proc);//if blocked -> High
-	if (current_proc->PID == 1) {//if ini P -> Low
-		current_proc->status = 0;
-		ListPrepend(ReadyQ_Low, current_proc);
+	if(current_proc != NULL){
+		printf("here!!!\n");
+		if (current_proc->status == -1) ListPrepend(ReadyQ_High, current_proc);//if blocked -> High
+		if (current_proc->PID == 1) {//if ini P -> Low
+			current_proc->status = 0;
+			ListPrepend(ReadyQ_Low, current_proc);
+		}
+		else {//if not blocked -> Norm
+			ListPrepend(ReadyQ_Norm, current_proc);
+			current_proc->status = 0;
+		}
 	}
-	else {//if not blocked -> Norm
-		ListPrepend(ReadyQ_Norm, current_proc);
-		current_proc->status = 0;
-
-	}
-
 	a = ListCount(ReadyQ_High); //Get High priority process
 	while(a != 0){
 		current_proc = ListTrim(ReadyQ_High);
@@ -85,6 +109,47 @@ void Quantum(){
 
 }
 
+void Kill(int PID){
+	PCB * to_be_killed = SearchAll(PID);
+
+	if(to_be_killed == NULL){
+		printf("Cannot find the PID to delete.\n");
+		return;
+	}
+
+	if (to_be_killed == current_proc) {
+		if(to_be_killed->priority == 2){
+			if(proc_count != 1) {
+				printf("Cannot Kill initial process, other process(es) still running. \n");
+				return;
+			}	
+			ListRemove(ReadyQ_Low);
+			proc_count--;
+			exit(1);
+		}
+		free(current_proc);
+		current_proc = NULL;
+		proc_count--;
+		Quantum();
+		return;
+	}
+	if(to_be_killed->priority == 0) ListRemove(ReadyQ_High);
+	else if(to_be_killed->priority == 1) ListRemove(ReadyQ_Norm);
+	else if(to_be_killed->priority == 2){
+		if(proc_count != 1) {
+			printf("Cannot Kill initial process, other process(es) still running. \n");
+			return;
+		}	
+		ListRemove(ReadyQ_Low);
+		exit(1);
+	}
+	proc_count--;
+}
+
+void Exit(){
+	Kill(current_proc->PID);
+}
+
 void Send(int PID, char * msg){
 
 }
@@ -109,32 +174,19 @@ void SemaphoreV(int SID){
 
 }
 
+
+
 void Proinfo(int PID){
 	printf("----------------------------------------------------------------------\n");
 	
-	PCB * Pinfo;
-
-	if(PID == current_proc->PID) {
-		Pinfo = current_proc;
-	}else {
-		ListFirst(ReadyQ_High);
-		Pinfo = ListSearch(ReadyQ_High, comparator, &PID);
-		if (Pinfo == NULL) {
-			printf("here\n");
-			ListFirst(ReadyQ_Norm);
-			Pinfo = ListSearch(ReadyQ_Norm, comparator, &PID);
-			if(Pinfo == NULL){
-				ListFirst(ReadyQ_Low);
-				Pinfo = ListSearch(ReadyQ_Low, comparator, &PID);
-			}
-		}
-	}
+	PCB * Pinfo = SearchAll(PID);
 	if (Pinfo == NULL){
 		printf("Process not found\n.");
 		return;
 	}
 	printPCB(Pinfo);
 }
+
 
 void TotalInfo(){
 	printf("----------------------------------------------------------------------");
@@ -194,12 +246,18 @@ int main(){
 				create(1,0);//create(priority, status)
 				break;
 			case 'F':
+				Fork();
 				break;
 
 			case 'K':
+				printf("Please enter the PID to Kill: ");
+				scanf("%d", &parameter);
+				getchar();
+				Kill(parameter);
 				break;
 
 			case 'E':
+				Exit();
 				break;
 
 			case 'Q':
